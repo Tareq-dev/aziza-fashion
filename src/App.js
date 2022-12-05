175a1287e973fb10af00fcc31bd51ac95f9ebb80
Tareq-dev <tareque.dev@gmail.com>
2022-12-05 09:24:21 -0600

    Data added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 // import Home from './Pages/Home/Home';
 import Products from './Pages/Products/Products';
+import SingleProducts from './Pages/Products/SingleProducts';
 function App() {
   return (
     <div>
       {/* <Home /> */}
-      <Products />
+      {/* <Products /> */}
+      <SingleProducts />
     </div>
   );
 }
--- src/Pages/Products/Product.js
+++ src/Pages/Products/Product.js
@@ -6,11 +6,11 @@ function Product({product}) {
 
     return (
             <div className="p-4 relative">
-                <div className="h-full border-2 border-gray-300 border-opacity-60 rounded-lg overflow-hidden">
-                    <div className='absolute top-10 right-10 bg-slate-100 p-2 shadow-sm rounded'>
+                <div className="h-full border-2 border-gray-300 border-opacity-60 rounded-lg shadow-md overflow-hidden">
+                    <div className='absolute top-10 right-10 bg-slate-100 p-2  rounded'>
                         <IoMdHeartEmpty size={30} />
                     </div>
-                    <img className="lg:h-96 md:h-36 w-full object-cover object-center" src="https://scontent.fcgp6-1.fna.fbcdn.net/v/t1.15752-9/315524486_836695860912993_2037433397284208238_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeHh_yIUzlAfQVsZUVUdlYozxtcXWqKBGHjG1xdaooEYeOqKN9uYsQXibI3Qn7UEQjT1MZlHxw6Kq6vIGy2SK_QY&_nc_ohc=4mvM32JFaQwAX9do5AG&_nc_pt=1&_nc_ht=scontent.fcgp6-1.fna&oh=03_AdRjXjDpxBVO_3zUzqwzplqSlOEjCt0pKgfvDQNXHyOstQ&oe=63B2412B" alt="blog" />
+                    <img className="lg:h-96 md:h-36 w-full object-cover object-center" src={product.picture} alt="blog" />
                     <div className="p-2">
                         <h1 className="title-font text-lg font-bold text-gray-900  uppercase px-2">{product.name}</h1>
                         <div className="flex items-center justify-between py-6 px-2">
