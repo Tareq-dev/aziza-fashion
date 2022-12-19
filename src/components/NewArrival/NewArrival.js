374aefad872863b02d5406c300ab7c92457c457b
Tareq-dev <tareque.dev@gmail.com>
2022-12-19 22:07:45 +0600

    Responsive Done

--- src/components/NewArrival/NewArrival.js
+++ src/components/NewArrival/NewArrival.js
@@ -18,9 +18,9 @@ function NewArrival() {
                         <img className='md:w-56 md:h-48 md:ml-10 my-1 md:my-0' src="https://img.freepik.com/free-photo/stylish-woman-with-shopping-bags_23-2148733284.jpg?w=740&t=st=1670042378~exp=1670042978~hmac=1f6b50c3ffd363eb152694dc95e458eaf6bfb5f56ce78afc634fb952964772b0" alt="" />
                     </div>
                     <div className='flex w-full flex-col md:flex-row mt-0 md:mt-8'>
-                        <img className='md:w-56 md:h-48 md:ml-10 my-1 md:my-0' src="https://img.freepik.com/free-photo/stylish-woman-with-shopping-bags_23-2148733284.jpg?w=740&t=st=1670042378~exp=1670042978~hmac=1f6b50c3ffd363eb152694dc95e458eaf6bfb5f56ce78afc634fb952964772b0" alt="" />
-                        <img className='md:w-56 md:h-48 md:ml-10 my-1 md:my-0' src="https://img.freepik.com/free-photo/stylish-woman-with-shopping-bags_23-2148733284.jpg?w=740&t=st=1670042378~exp=1670042978~hmac=1f6b50c3ffd363eb152694dc95e458eaf6bfb5f56ce78afc634fb952964772b0" alt="" />
-                        <img className='md:w-56 md:h-48 md:ml-10 my-1 md:my-0' src="https://img.freepik.com/free-photo/stylish-woman-with-shopping-bags_23-2148733284.jpg?w=740&t=st=1670042378~exp=1670042978~hmac=1f6b50c3ffd363eb152694dc95e458eaf6bfb5f56ce78afc634fb952964772b0" alt="" />
+                        <img className='md:w-56 md:h-48 md:mr-10 my-1 md:my-0' src="https://img.freepik.com/free-photo/stylish-woman-with-shopping-bags_23-2148733284.jpg?w=740&t=st=1670042378~exp=1670042978~hmac=1f6b50c3ffd363eb152694dc95e458eaf6bfb5f56ce78afc634fb952964772b0" alt="" />
+                        <img className='md:w-56 md:h-48 md:mr-10 my-1 md:my-0' src="https://img.freepik.com/free-photo/stylish-woman-with-shopping-bags_23-2148733284.jpg?w=740&t=st=1670042378~exp=1670042978~hmac=1f6b50c3ffd363eb152694dc95e458eaf6bfb5f56ce78afc634fb952964772b0" alt="" />
+                        <img className='md:w-56 md:h-48 md:mr-10 my-1 md:my-0' src="https://img.freepik.com/free-photo/stylish-woman-with-shopping-bags_23-2148733284.jpg?w=740&t=st=1670042378~exp=1670042978~hmac=1f6b50c3ffd363eb152694dc95e458eaf6bfb5f56ce78afc634fb952964772b0" alt="" />
                     </div>
                 </div>
             </div>
