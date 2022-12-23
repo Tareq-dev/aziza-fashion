81a7e1c66f05c7b33620f4587e528d9eb04ca030
Tareq-dev <tareque.dev@gmail.com>
2022-12-23 12:05:53 +0600

    Product Add

--- src/Pages/Products/SingleProducts.js
+++ src/Pages/Products/SingleProducts.js
@@ -20,8 +20,8 @@ function SingleProducts({ addToCard }) {
         <div className="text-gray-600 body-font overflow-hidden">
             <div className="container px-5 py-24 mx-auto">
                 <div className="lg:w-4/5 mx-auto flex flex-wrap">
-                    <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src={product.picture} />
-                    <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                    <img alt="ecommerce" className="md:w-1/2 w-full md:h-full object-cover object-center rounded" src={product.picture} />
+                    <div className="lg:w-1/2 w-full lg:px-10 mt-6 lg:mt-0 py-20">
                         <h2 className="text-sm title-font text-gray-500 tracking-widest uppercase">Brand : {product?.brand}</h2>
                         <h1 className="text-gray-900 text-3xl title-font font-medium mb-1"> {product.name}</h1>
                         <p className='text-pink-500 font-bold'>{product?.rating} Review</p>
